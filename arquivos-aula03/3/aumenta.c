6e6f5603bb20e6089360392e0346b050e0738cde
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2020-03-10 19:22:08 -0300

    3 feit

---
+++ arquivos-aula03/3/aumenta.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    FILE *arquivo1, *arquivo2;
+    arquivo1 = fopen("minusc.txt", "r");
+    arquivo2 = fopen("maiusc.txt", "w");
+    
+    char c;
+
+    c = fgetc(arquivo1);
+    
+    while(c != EOF){
+    
+        if (c != 32)
+            fputc(c-32, arquivo2);
+        else 
+            fputc(c, arquivo2);
+        c = fgetc(arquivo1);
+    
+    }
+
+    fclose(arquivo1);
+    fclose(arquivo2);
+
+    return 0;
+}
